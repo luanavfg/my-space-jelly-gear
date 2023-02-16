c6272e40da7e1b3dd1c48eb4aadc66bbeca0b385
luanavfg <luanavefago1@gmail.com>
2023-02-16 09:54:25 -0300

    feat: lesson 12 - add featured category products in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -93,7 +93,8 @@ export async function getStaticProps() {
           slug
           heroBackground
         }
-        products(first: 4) {
+        products(where: {categories_some: {slug: "featured"}}) {
+          id
           name
           price
           slug
